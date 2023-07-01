5a661bce3e9910059de63f7d7f53206941556a8c
victor <victor@nexusrobotics.ca>
2023-07-01 16:04:02 -0400

    fixed train.py

--- train.py
+++ train.py
@@ -4,19 +4,19 @@ import json
 from ultralytics import YOLO
 
 convert_coco(labels_dir='datasets/', cls91to80=False)
-# shutil.move("val", "./yolo_labels/images/val")
-# shutil.move("train", "./yolo_labels/images/train")
 with open("datasets/train.json", "r") as f:
     coco_json = json.load(f)
-yaml_content = """path: ./yolo_labels
+yaml_content = """path: ./
 train: images/train
 val: images/val
 names:
 """
 for subclass in coco_json["categories"]:
     yaml_content += f"  {subclass['id']-1}: {subclass['name']}\n"
-with open("../yolo_labels/train.yaml", "w") as f:
+with open("./yolo_labels/train.yaml", "w") as f:
     f.write(yaml_content)
+
+shutil.move("./yolo_labels/labels", "./datasets/labels")
 
 # Load a model
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
